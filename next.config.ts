2a3f83d4a654ad0e47d686a4a3eebc8226ce3c46
SHO800 <fg582323@docomo.ne.jp>
2026-03-05 18:18:45 +0900

    GitHub Pages使用のための諸設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+// ここに作成したリポジトリ名を設定してください
+const repoName = 'sho800-3d-demo';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    // 静的エクスポートを有効化
+    output: 'export',
+
+    // プロジェクトサイトのURL（/リポジトリ名）に対応させる
+    basePath: `/${repoName}`,
+
+    images: {
+        // 静的エクスポートではNext.jsの画像最適化APIが使えないため無効化
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
